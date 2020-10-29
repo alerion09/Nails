7d324007e3c8dde875b90ce636ba1648e845cdcf
Kajetan Kuzma <kajetankuzma@gmail.com>
2020-10-29 16:26:31 +0100

    Created layout view for screen size between 440 and 320 px (except menu)

--- script_gallery.js
+++ script_gallery.js
@@ -1,7 +1,12 @@
+//GALLERY SCRIPT
 document.addEventListener("DOMContentLoaded", appStart)
 function appStart()
 {   
-    if (window.matchMedia("(max-width: 1023px)").matches)
+    if (window.matchMedia("(max-width: 439px)").matches)
+    {
+        create_gallery_columns(1);
+    }
+    else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
     {
         create_gallery_columns(2);
     }
@@ -11,14 +16,18 @@ function appStart()
     } 
     window.onresize = function() // Wywolanie funkcji zmiany kolumn przy kazdorazowej zmianie rozmiaru okna
     {
-        if (window.matchMedia("(max-width: 1023px)").matches)
+        if (window.matchMedia("(max-width: 439px)").matches)
+        {
+            create_gallery_columns(1);
+        }
+        else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
         {
             create_gallery_columns(2);
         }
         else
         {
             create_gallery_columns(3);
-        }     
+    }       
     }
     
 }
@@ -30,7 +39,14 @@ function create_gallery_columns (number) //Tworzenie wyznaczonej ilosci kolumn w
     var second_column = document.getElementById("gallery_second_column");
     var third_column = document.getElementById("gallery_third_column");
 
-    if(number == 2)
+    if (number == 1)
+    {
+        first_column.innerHTML = '<img src="./img/g1.jpg" alt="Nails."><img src="./img/g5.jpg" alt="Nails."><img src="./img/g6.jpg" alt="Nails."><img src="./img/g4.jpg" alt="Nails."><img src="./img/g11.jpg" alt="Nails."><img src="./img/g12.jpg" alt="Nails."><img src="./img/g3.jpg" alt="Nails."><img src="./img/g7.jpg" alt="Nails."><img src="./img/g8.jpg" alt="Nails."><img src="./img/g9.jpg" alt="Nails."><img src="./img/g10.jpg" alt="Nails."><img src="./img/g2.jpg" alt="Nails."><img src="./img/g14.jpg" alt="Nails."><img src="./img/g15.jpg" alt="Nails."><img src="./img/g13.jpg" alt="Nails.">';
+        second_column.innerHTML = "";
+        third_column.innerHTML = "";
+        console.log("Function create gallery columns work. Number of columns: "+number);
+    }
+    else if(number == 2)
     {
         first_column.innerHTML = '<img src="./img/g1.jpg" alt="Nails."><img src="./img/g5.jpg" alt="Nails."><img src="./img/g6.jpg" alt="Nails."><img src="./img/g4.jpg" alt="Nails."><img src="./img/g11.jpg" alt="Nails."><img src="./img/g12.jpg" alt="Nails.">';
         second_column.innerHTML = '<img src="./img/g3.jpg" alt="Nails."><img src="./img/g7.jpg" alt="Nails."><img src="./img/g8.jpg" alt="Nails."><img src="./img/g9.jpg" alt="Nails."><img src="./img/g10.jpg" alt="Nails."><img src="./img/g2.jpg" alt="Nails."><img src="./img/g14.jpg" alt="Nails."><img src="./img/g15.jpg" alt="Nails."><img src="./img/g13.jpg" alt="Nails.">';
