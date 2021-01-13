4b7b22db69d89d8eac9094222eb1298ceea849fd
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-13 23:42:53 +0100

    Disabled 'onresize' function at 'script_gallery'.

--- script_gallery.js
+++ script_gallery.js
@@ -17,28 +17,28 @@ function appStart()
         create_gallery_columns(3);
     }
     
-    window.onresize = function() //Funkcja wywolywana podczas zmiany rozmiaru okna
-    {   
-        const icon_element = document.getElementById("menu_icon");
-        if  (window.matchMedia("(max-width: 439px)").matches) 
-        {                                                                              
-            create_gallery_columns(1);
-            if (icon_element == null & (window.matchMedia("(max-width: 439px)").matches))
-            {
-                create_toggle_menu();
-            }
-        }
-        else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
-        {                                                                               
-            create_gallery_columns(2);
-            if (icon_element != null & window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
-            {
-                delete_toggle_menu();
-            }
-        }
-        else if (window.matchMedia("(min-width: 1024px)").matches)
-        {   
-            create_gallery_columns(3);
-        }
-    }
+    // window.onresize = function() //Funkcja wywolywana podczas zmiany rozmiaru okna
+    // {   
+    //     const icon_element = document.getElementById("menu_icon");
+    //     if  (window.matchMedia("(max-width: 439px)").matches) 
+    //     {                                                                              
+    //         create_gallery_columns(1);
+    //         if (icon_element == null & (window.matchMedia("(max-width: 439px)").matches))
+    //         {
+    //             create_toggle_menu();
+    //         }
+    //     }
+    //     else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
+    //     {                                                                               
+    //         create_gallery_columns(2);
+    //         if (icon_element != null & window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
+    //         {
+    //             delete_toggle_menu();
+    //         }
+    //     }
+    //     else if (window.matchMedia("(min-width: 1024px)").matches)
+    //     {   
+    //         create_gallery_columns(3);
+    //     }
+    // }
 }
