8428e7ca976cc8bcee6c0fe0f9049153c2360803
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-14 22:51:40 +0100

    Corrected matchMedia instructions in 'script_menu'.

--- script_menu.js
+++ script_menu.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", appStart)
 function appStart()
 {   
-    console.log("menu js start");
-    if (window.matchMedia("(max-width: 439px)").matches) //Jesli okno jest mniejsze od 439 to utworz menu
+    const mqueryMax439 = window.matchMedia("(max-width: 439px)");
+    const mqueryMin440 = window.matchMedia("(min-width: 440px)");
+    if (mqueryMax439.matches) //Jesli okno jest mniejsze od 439 to utworz menu
     {
         create_toggle_menu();
     }
-    
-    window.onresize = function() //Funkcja wywolywana podczas zmiany rozmiaru okna
-    {   
-        const icon_element = document.getElementById("menu_icon");
-        if(icon_element == null & (window.matchMedia("(max-width: 439px)").matches))   //Gdy element ikony menu nie istnieje, a rozmiar jest mniejszy 
-        {                                                                               //niz 439 wywolaj funkcje tworzaca iteraktywne menu 
+    mqueryMax439.addEventListener("change",(e) => {
+        if(e.matches)
+        {
             create_toggle_menu();
-        }else if (icon_element != null & window.matchMedia("(min-width: 440px)").matches)//Gdy element ikony Istnieje, a rozmiar jest wiekszy
-        {                                                                                  //niz 439 wywolaj funkcje usuwajaca iteraktywne menu
+        }
+    });
+    mqueryMin440.addEventListener("change", (e) => {
+        if(e.matches)
+        {
             delete_toggle_menu();
         }
-    }
+    });
 }
