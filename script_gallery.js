7302852016319b55e99d092401babe63557b9214
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-14 22:11:36 +0100

    Created addEventListener for match media in 'script_gallery'.

--- script_gallery.js
+++ script_gallery.js
@@ -2,43 +2,36 @@
 document.addEventListener("DOMContentLoaded", appStart)
 function appStart()
 {   
-    if (window.matchMedia("(max-width: 439px)").matches) //Jesli okno jest mniejsze od 439 to utworz menu
-    {
-        create_toggle_menu();
-        console.log("menu js start");
-        create_gallery_columns(1);
-    }
-    else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
-    {
-        create_gallery_columns(2);
-    }
-    else
-    {
-        create_gallery_columns(3);
-    }
+    const queryMax439 = window.matchMedia("(max-width: 439px)");
+    const querymin440andmax1023 = window.matchMedia("(min-width: 440px) and (max-width: 1023px)");
+    const queryMin1024 = window.matchMedia("(min-width: 1024px)");
     
-    // window.onresize = function() //Funkcja wywolywana podczas zmiany rozmiaru okna
-    // {   
-    //     const icon_element = document.getElementById("menu_icon");
-    //     if  (window.matchMedia("(max-width: 439px)").matches) 
-    //     {                                                                              
-    //         create_gallery_columns(1);
-    //         if (icon_element == null & (window.matchMedia("(max-width: 439px)").matches))
-    //         {
-    //             create_toggle_menu();
-    //         }
-    //     }
-    //     else if (window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
-    //     {                                                                               
-    //         create_gallery_columns(2);
-    //         if (icon_element != null & window.matchMedia("(min-width: 440px) and (max-width: 1023px)").matches)
-    //         {
-    //             delete_toggle_menu();
-    //         }
-    //     }
-    //     else if (window.matchMedia("(min-width: 1024px)").matches)
-    //     {   
-    //         create_gallery_columns(3);
-    //     }
-    // }
+    queryMax439.addEventListener("change", (e) => {
+        if (e.matches)
+        {
+            const icon_element = document.getElementById("menu_icon");
+            create_gallery_columns(1);
+            if (icon_element == null )
+            {
+                create_toggle_menu();
+            }
+        }
+    });
+    querymin440andmax1023.addEventListener("change", (e) => {
+        if (e.matches)
+        {   
+            const icon_element = document.getElementById("menu_icon");
+            create_gallery_columns(2);
+            if (icon_element != null )
+            {
+                delete_toggle_menu();
+            }
+        }
+    });
+    queryMin1024.addEventListener("change", (e) => {
+        if (e.matches)
+        {
+            create_gallery_columns(3);
+        }
+    });
 }
