c15c3165c97b22e866e895a4d90fd8a89cc010be
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-14 22:32:06 +0100

    Corrected menu display in gallery.

--- script_gallery.js
+++ script_gallery.js
@@ -5,16 +5,15 @@ function appStart()
     const queryMax439 = window.matchMedia("(max-width: 439px)");
     const querymin440andmax1023 = window.matchMedia("(min-width: 440px) and (max-width: 1023px)");
     const queryMin1024 = window.matchMedia("(min-width: 1024px)");
-    
+    if(queryMax439.matches)
+    {
+        create_toggle_menu();
+    }
     queryMax439.addEventListener("change", (e) => {
         if (e.matches)
         {
-            const icon_element = document.getElementById("menu_icon");
+            create_toggle_menu();
             create_gallery_columns(1);
-            if (icon_element == null )
-            {
-                create_toggle_menu();
-            }
         }
     });
     querymin440andmax1023.addEventListener("change", (e) => {
